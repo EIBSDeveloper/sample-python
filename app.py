9a5a0b01dbc5582cab81773e8550094d4036bd7a
priya <test@gmail.com>
2026-08-28 16:10:10 +0530

    pD

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ PORT = int(os.getenv("PORT", 5000))
 def home():
     return jsonify({
         "status": "success",
-        "messageesdsdsdsds": f"{APP_NAME} is running!",
+        "message": f"{APP_NAME} is running!",
         "technologye": "Python Flask",
         "servere": "AWS EC2"
     })
